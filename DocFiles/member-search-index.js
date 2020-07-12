531202e6842803708dd38ce7d82023254a1b80c8
AnuragBarfa <anuragbarfa64@gmail.com>
2020-07-12 12:56:23 +0530

    feat+docs : Add interface CircularObject and documentation 	to remove the dependency of gear to get diameter from Wheel only as in future other object can also respond to this message implemented a interface which responds to diameter message and wheel class implement this interface in present

---
+++ DocFiles/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"learnDesign","c":"OODesign","l":"main(String[])","url":"main(java.lang.String[])"},{"p":"learnDesign","c":"OODesign","l":"OODesign()","url":"%3Cinit%3E()"}]
--- OODesign.java
+++ OODesign.java
@@ -1,35 +1,80 @@
 package learnDesign;
-class Wheel{
+
+/**
+* An <code>CircularObject</code> interface give abstratc behaviour for circular objects
+* @author Anurag Barfa
+* @version1.1
+*/
+interface CircularObject{
+	/**
+	* @return (int)diameter   
+	*/
+	public int diameter();
+}
+/**
+* An <code>Wheel</code> object store information of rim, tire dimensions
+* @author Anurag Barfa
+* @version1.1
+*/
+class Wheel implements CircularObject{
 	private int rim;
 	private int tire;
 	public Wheel(int rim,int tire){
 		this.rim=rim;
 		this.tire=tire;
 	}
-	public int diameter(){
-		return rim+2*tire;
+	/**
+	* Calculate rim of wheel
+	* @return (int)rim  
+	*/
+	public int getRim(){
+		return this.rim;
 	}
+	/**
+	* Calculate diameter of wheel
+	* @return (int)diameter  
+	*/
+	public int diameter(){
+		return getRim()+2*tire;
+	}
+	/**
+	* Calculate circumference of wheel
+	* @return (int)circumference  
+	*/
 	public double circumference(){
 		return diameter()*3.14;
 	}
 }
+/**
+* An <code>Gear</code> object store information of chain,cog, wheel
+* @author Anurag Barfa
+* @version1.1
+*/
 class Gear{
 	private int chainring;
 	private int cog;
-	private Wheel wheel;
-	public Gear(int chainring,int cog,Wheel wheel){
+	private CircularObject wheel;
+	public Gear(int chainring,int cog,CircularObject wheel){
 		this.chainring=chainring;
 		this.cog=cog;
 		this.wheel=wheel;
 	}
+	/**
+	* Calculate ratio for gear
+	* @return (double)ratio  
+	*/
 	public double ratio(){
 		return (double)this.chainring/(double)this.cog;
 	}
+	/**
+	* Calculate gear_inches for gear
+	* @return (double)gear_inches  
+	*/
 	public double gear_inches(){
 		return ratio()*wheel.diameter();
 	}
 }
-class OODesign{
+public class OODesign{
 	public static void main(String[] args){
 		Wheel wheel=new Wheel(26,2);
 		Gear gear=new Gear(12,7,wheel);
